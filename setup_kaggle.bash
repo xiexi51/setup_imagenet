7968276370e0ac8884434b25c8aecfd599d3918b
xiexi1990 <xiexi_1990@126.com>
2024-03-21 01:13:19 +0000

    echo before and after unzip

--- setup_kaggle.bash
+++ setup_kaggle.bash
@@ -3,5 +3,7 @@ mkdir -p ~/.kaggle
 cp ./kaggle.json ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
 kaggle competitions download -c imagenet-object-localization-challenge
+echo "unzip..."
 unzip imagenet-object-localization-challenge.zip > /dev/null 2>&1
+echo "unzip finish"
 python process_kaggle_val.py
