a18212755e9c8926d46f40adb6e299609478dc71
xiexi1990 <xiexi_1990@126.com>
2024-03-22 00:12:01 +0000

    add ip_list

--- setup_next.bash
+++ setup_next.bash
@@ -4,3 +4,4 @@ cd ..
 git clone https://github.com/harveyp123/HE_transfer_learning.git
 cp ./setup_imagenet/locals.py ./HE_transfer_learning
 chmod +x ./HE_transfer_learning/run_ddp.bash
+cp ./setup_imagenet/ip_list ./HE_transfer_learning
