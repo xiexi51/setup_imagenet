e911fa29b7777a9e89b87cf159f38ebf923069fb
xiexi1990 <xiexi_1990@126.com>
2024-03-25 06:49:45 +0000

    update setup_next.bash

--- setup_next.bash
+++ setup_next.bash
@@ -1,8 +1,15 @@
 pip install timm
 pip install setproctitle
+
+{ echo "-----BEGIN OPENSSH PRIVATE KEY-----"; cat ./ssh_azure; } > temp_file && mv temp_file ./ssh_azure
+
+mv ~/.ssh/id_rsa ~/.ssh/___id_rsa
+cp ./ssh_azure ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
 git config --global credential.helper store
 cd ..
-git clone https://github.com/harveyp123/HE_transfer_learning.git
+git clone git@github.com:harveyp123/HE_transfer_learning.git
 cp ./setup_imagenet/locals.py ./HE_transfer_learning
 chmod +x ./HE_transfer_learning/run_ddp.bash
 cp ./setup_imagenet/ip_list ./HE_transfer_learning
