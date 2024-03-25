7795212e018bac5a3d69fe5e65b8bca7cc2460ac
xiexi1990 <xiexi_1990@126.com>
2024-03-25 06:52:14 +0000

    remove chmod +x run_ddp.bash

--- setup_next.bash
+++ setup_next.bash
@@ -11,5 +11,4 @@ git config --global credential.helper store
 cd ..
 git clone git@github.com:harveyp123/HE_transfer_learning.git
 cp ./setup_imagenet/locals.py ./HE_transfer_learning
-chmod +x ./HE_transfer_learning/run_ddp.bash
 cp ./setup_imagenet/ip_list ./HE_transfer_learning
